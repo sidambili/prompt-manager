00de813d76a11e0bd7cceeac0784550cbb57062a
Sid <124750078+sidambili@users.noreply.github.com>
2025-12-31 00:24:06 -0500

    refactor: replace custom Switch component with Radix UI implementation from shadcn

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,26 +1,31 @@
-'use client';
+"use client"
 
-import * as React from 'react';
+import * as React from "react"
+import * as SwitchPrimitive from "@radix-ui/react-switch"
 
-type SwitchProps = Omit<React.InputHTMLAttributes<HTMLInputElement>, 'type'> & {
-  checked: boolean;
-  onCheckedChange: (checked: boolean) => void;
-};
+import { cn } from "@/lib/utils"
 
-const Switch = React.forwardRef<HTMLInputElement, SwitchProps>(
-  ({ checked, onCheckedChange, ...props }, ref) => {
-    return (
-      <input
-        {...props}
-        ref={ref}
-        type="checkbox"
-        checked={checked}
-        onChange={(e) => onCheckedChange(e.target.checked)}
+function Switch({
+  className,
+  ...props
+}: React.ComponentProps<typeof SwitchPrimitive.Root>) {
+  return (
+    <SwitchPrimitive.Root
+      data-slot="switch"
+      className={cn(
+        "peer data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-input/80 inline-flex h-[1.15rem] w-8 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
+      {...props}
+    >
+      <SwitchPrimitive.Thumb
+        data-slot="switch-thumb"
+        className={cn(
+          "bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block size-4 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0"
+        )}
       />
-    );
-  }
-);
+    </SwitchPrimitive.Root>
+  )
+}
 
-Switch.displayName = 'Switch';
-
-export { Switch };
+export { Switch }
